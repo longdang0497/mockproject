513544689d42498f2456fdf7d250ad9491cd35ef
longdang0497 <danghoanglong3004@gmail.com>
2019-07-25 15:34:52 +0700

    Fix active admin cho blog và experience

--- app/admin/blogs.rb
+++ app/admin/blogs.rb
@@ -12,22 +12,35 @@ ActiveAdmin.register Blog do
   #   permitted << :other if params[:action] == 'create' && current_user.admin?
   #   permitted
   # end
-  permit_params :admin_user_id, :location_id
+
+  permit_params :title, :short_description, :content, :admin_user_id, :location_id
+
   index do
     selectable_column
     column :id
+    column :title 
+    column :short_description
+    column :content
     column "Location" do |i|
       i.location.province
     end
-    column :impressions_count  
-    column :created_at
-    column :updated_at  
+    column "Views", :impressions_count  
+    column "Created", :created_at
+    column "Updated", :updated_at  
     actions
   end
+
   show do
     attributes_table do
+      row :id
+      row :title
+      row :short_description
+      row :content
       row "Location" do |i|
         i.location.province
+      end
+      row "Created by" do |i|
+        i.admin_user.fullname
       end
       row "Admin user" do |i|
         i.admin_user.email
@@ -39,13 +52,22 @@ ActiveAdmin.register Blog do
     active_admin_comments
   end
 
-  # filter :location, :collection => Location.all.collect {|loca| [loca.province, loca.id] }
-  # filter :admin_user, :collection => AdminUser.all.collect {|ad| [ad.email, ad.id] }
+  filter :id
+  filter :title
+  filter :short_description
+  filter :content
+  filter :location, :collection => Location.all.collect {|loca| [loca.province, loca.id] }
+  filter :admin_user, :as => :select, :collection => AdminUser.all.collect {|ad| [ad.fullname, ad.id] }
+  filter :created_at
+  filter :updated_at
 
   form do |f|
     f.inputs do
+      f.input :title 
+      f.input :content,  :as => :ckeditor
       f.input :location, :as => :select, :collection => Location.all.collect {|loca| [loca.province, loca.id] }
-      f.input :admin_user, :as => :select, :collection => AdminUser.all.collect {|ad| [ad.email, ad.id] }
+      f.input :admin_user, :as => :select, :collection => AdminUser.all.collect {|ad| [ad.fullname, ad.id] }
+      # f.input :hashtag
     end
     f.actions
   end
