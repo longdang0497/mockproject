2203df33e7a98e71ae1070c82d4ec91397ba5cf5
duongtrang2701 <duong1997trang@gmail.com>
2019-07-30 08:47:32 +0700

    Multi language

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,11 @@
 
 class ApplicationController < ActionController::Base
   # breadcrumb root
+  before_action :set_i18n_locale_from_params
   protect_from_forgery
-  add_breadcrumb 'Top', :top_path
+  add_breadcrumb I18n.t("breadcrumbs.top"), :top_path
 
   before_action :configure_permitted_parameters, if: :devise_controller?
-
   protected
 
   def configure_permitted_parameters
@@ -20,5 +20,18 @@ class ApplicationController < ActionController::Base
   def access_denied(exception)
     redirect_to top_path, alert: exception.message
   end
-
+  protected
+    def set_i18n_locale_from_params
+      if params[:locale]
+        if I18n.available_locales.map(&:to_s).include?(params[:locale])
+          I18n.locale = params[:locale]
+        else
+          flash.now[:notice] = "#{params[:locale]} translation not available"
+          logger.error flash.now[:notice]
+        end
+      end
+    end
+    def default_url_options
+      { locale: I18n.locale }
+    end
 end
